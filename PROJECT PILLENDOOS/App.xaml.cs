667100e072b99288d81093505381aceae8122674
Ziggy Van Gyseghem <ziggy.vangyseghem@student.vives.be>
2024-12-09 12:53:34 +0100

    code

---
+++ PROJECT PILLENDOOS/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROJECT_PILLENDOOS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ PROJECT PILLENDOOS/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PROJECT_PILLENDOOS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+     public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+        }
+        
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            string time = currentTime.ToString("HH:mm:ss");
+            LBLTime.Content = time;
+            LED1.Fill = new SolidColorBrush(Colors.Red);
+            LED2.Fill = new SolidColorBrush(Colors.Red);
+            LED3.Fill = new SolidColorBrush(Colors.Red);
+            LED4.Fill = new SolidColorBrush(Colors.Red);
+
+        }
+    }
+}
